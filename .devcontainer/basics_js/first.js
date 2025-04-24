3ebd5debb8eb3d45d714aa8bf735fb732b3b9d46
Shivanshi Sugandhi <shivanshisugandhi@gmail.com>
2025-04-24 18:20:36 +0000

    first.js

--- .devcontainer/basics_js/first.js
+++ .devcontainer/basics_js/first.js
@@ -1,1 +1,8 @@
 console.log("shivanshi");
+
+const accounId = 27188;
+let accountEmail = "@";
+var accountPassword = "618887";
+accountCity = "jaiput";
+
+console.table([accounId,accountEmail,accountPassword,accountCity]);
